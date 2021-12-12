fdedb60e5f84c9f97cf677e77ecbaad84e369c99
dustin <eiki300499@gmail.com>
2021-12-12 18:25:43 +0100

    init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AppLinux;
+using ConsoleApplication.Vehicles;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Person carl = new CarDriver();
+            carl.setFirstname("Carl");
+            carl.setLastname("Benz");
+            carl.setSex('m');
+            carl.setAge(56);
+            Console.WriteLine(((Driver) carl).getCars().Count);
+            ((Driver) carl).getCars().Add(car1);
+            Console.WriteLine(carl.ToString());
+            Console.WriteLine(((Driver) carl).getCars().Count);
+        }
+    }
+}
